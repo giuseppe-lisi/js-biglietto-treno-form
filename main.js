17e762cf61266258e2ddd7ff452ecf3eaef14152
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-12 00:19:16 +0100

    completato esercizio

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ form.addEventListener("submit", (event) => {
 
     event.preventDefault();
 
+    //form utente
     let nameEl = document.getElementById("name").value.trim();
-    let ageEl = document.getElementById("age").value;
     let kmEl = parseInt(document.getElementById("km").value);
+    let ageEl = document.getElementById("age").value;
     
+    //calcolo prezzo base
     let price = kmEl * 0.21;
+
+    //elementi biglietto
     let passenger = document.getElementById("passenger");
     let offer = document.getElementById("offer");
     let finalPrice = document.getElementById("price");
 
+    //controllo discount
     if (ageEl === "Minorenne") {
         price = price * 0.80;
     }
@@ -20,8 +25,9 @@ form.addEventListener("submit", (event) => {
         price = price * 0.60;
     }
 
+    //scrivo dati sul biglietto
+    finalPrice.innerHTML = `€ ${price.toFixed(2)}`;
     passenger.innerText = nameEl;
     offer.innerText = ageEl
-    finalPrice.innerHTML = `€ ${price.toFixed(2)}`;
 });
 
