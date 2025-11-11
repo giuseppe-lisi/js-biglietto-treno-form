e4fc77511e59773ec5db3a95d7685edfe7618d1d
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-11 22:25:46 +0100

    creato form di base con bottone

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-let km = parseInt(prompt("Quanti Km devi percorrere?"));
-let age = parseInt(prompt("Quanti anni hai?"));
-let pricePerKm = 0.21;
-let ticketPrice = km * pricePerKm;
+// let km = parseInt(prompt("Quanti Km devi percorrere?"));
+// let age = parseInt(prompt("Quanti anni hai?"));
+// let pricePerKm = 0.21;
+// let ticketPrice = km * pricePerKm;
 
-if (age < 18) {
-    ticketPrice = ticketPrice * 0.80;
-}
-if (age >= 65) {
-    ticketPrice = ticketPrice * 0.60;
-}
+// if (age < 18) {
+//     ticketPrice = ticketPrice * 0.80;
+// }
+// if (age >= 65) {
+//     ticketPrice = ticketPrice * 0.60;
+// }
 
-alert(`Costo biglietto = ${ticketPrice}`);
+// alert(`Costo biglietto = ${ticketPrice}`);
+
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", (event) => {
+
+    event.preventDefault();
+
+});
